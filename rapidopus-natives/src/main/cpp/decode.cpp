ffb97fb828541a6267221acaafedeb5a761e88c6
Walker Knapp <walker@walkerknapp.me>
2021-07-21 19:33:38 -0400

    Correctly annotate JNI function implementations

--- rapidopus-natives/src/main/cpp/decode.cpp
+++ rapidopus-natives/src/main/cpp/decode.cpp
@@ -1,9 +1,9 @@
 #include <jni.h>
 
 #include "com_walker_rapidopus_OpusDecoder.h"
-#include <opus.h>
+#include <opus/opus.h>
 
-jlong Java_com_walker_rapidopus_OpusDecoder_decoderCreate(JNIEnv *env, jclass jClazz, jint jSampleRate, jint jChannels) {
+JNIEXPORT jlong JNICALL Java_com_walker_rapidopus_OpusDecoder_decoderCreate(JNIEnv *env, jclass jClazz, jint jSampleRate, jint jChannels) {
     int err = OPUS_OK;
 
     auto *decoder = opus_decoder_create(jSampleRate, jChannels, &err);
@@ -15,11 +15,11 @@ jlong Java_com_walker_rapidopus_OpusDecoder_decoderCreate(JNIEnv *env, jclass jC
     return reinterpret_cast<jlong>(decoder);
 }
 
-void Java_com_walker_rapidopus_OpusDecoder_decoderDestroy(JNIEnv *env, jclass jClazz, jlong pDecoder) {
+JNIEXPORT void JNICALL Java_com_walker_rapidopus_OpusDecoder_decoderDestroy(JNIEnv *env, jclass jClazz, jlong pDecoder) {
     opus_decoder_destroy(reinterpret_cast<OpusDecoder *>(pDecoder));
 }
 
-jint Java_com_walker_rapidopus_OpusDecoder_decode__J_3BLjava_nio_ByteBuffer_2II
+JNIEXPORT jint JNICALL Java_com_walker_rapidopus_OpusDecoder_decode__J_3BLjava_nio_ByteBuffer_2II
     (JNIEnv *env, jclass jClazz, jlong pDecoder, jbyteArray jInputData, jobject jOutBuffer, jint jFrameSize, jint jDecodeFec) {
 
     jboolean isCopy = JNI_FALSE;
@@ -35,7 +35,7 @@ jint Java_com_walker_rapidopus_OpusDecoder_decode__J_3BLjava_nio_ByteBuffer_2II
     return ret;
 }
 
-jint Java_com_walker_rapidopus_OpusDecoder_decode__JLjava_nio_ByteBuffer_2Ljava_nio_ByteBuffer_2II
+JNIEXPORT jint JNICALL Java_com_walker_rapidopus_OpusDecoder_decode__JLjava_nio_ByteBuffer_2Ljava_nio_ByteBuffer_2II
     (JNIEnv *env, jclass jClazz, jlong pDecoder, jobject jInputBuffer, jobject jOutputBuffer, jint jFrameSize, jint jDecodeFec) {
 
     auto *inData = reinterpret_cast<unsigned char *>(env->GetDirectBufferAddress(jInputBuffer));
@@ -45,7 +45,7 @@ jint Java_com_walker_rapidopus_OpusDecoder_decode__JLjava_nio_ByteBuffer_2Ljava_
     return opus_decode(reinterpret_cast<OpusDecoder *>(pDecoder), inData, inSize, outData, jFrameSize, jDecodeFec);
 }
 
-jint Java_com_walker_rapidopus_OpusDecoder_decodeFloat__J_3BLjava_nio_ByteBuffer_2II
+JNIEXPORT jint JNICALL Java_com_walker_rapidopus_OpusDecoder_decodeFloat__J_3BLjava_nio_ByteBuffer_2II
     (JNIEnv *env, jclass jClazz, jlong pDecoder, jbyteArray jInputData, jobject jOutputBuffer, jint jFrameSize, jint jDecodeFec) {
 
     jboolean isCopy = JNI_FALSE;
@@ -61,7 +61,7 @@ jint Java_com_walker_rapidopus_OpusDecoder_decodeFloat__J_3BLjava_nio_ByteBuffer
     return ret;
 }
 
-jint Java_com_walker_rapidopus_OpusDecoder_decodeFloat__JLjava_nio_ByteBuffer_2Ljava_nio_ByteBuffer_2II
+JNIEXPORT jint JNICALL Java_com_walker_rapidopus_OpusDecoder_decodeFloat__JLjava_nio_ByteBuffer_2Ljava_nio_ByteBuffer_2II
     (JNIEnv *env, jclass jClazz, jlong pDecoder, jobject jInputBuffer, jobject jOutputBuffer, jint jFrameSize, jint jDecodeFec) {
 
     auto *inData = reinterpret_cast<unsigned char *>(env->GetDirectBufferAddress(jInputBuffer));
@@ -71,7 +71,7 @@ jint Java_com_walker_rapidopus_OpusDecoder_decodeFloat__JLjava_nio_ByteBuffer_2L
     return opus_decode_float(reinterpret_cast<OpusDecoder *>(pDecoder), inData, inSize, outData, jFrameSize, jDecodeFec);
 }
 
-jint Java_com_walker_rapidopus_OpusDecoder_decoderGetNbSamples__J_3B(JNIEnv *env, jclass jClazz, jlong pDecoder, jbyteArray jInputData) {
+JNIEXPORT jint JNICALL Java_com_walker_rapidopus_OpusDecoder_decoderGetNbSamples__J_3B(JNIEnv *env, jclass jClazz, jlong pDecoder, jbyteArray jInputData) {
     jboolean isCopy = JNI_FALSE;
     int inSize = env->GetArrayLength(jInputData);
     jbyte *inData = env->GetByteArrayElements(jInputData, &isCopy);
@@ -83,14 +83,14 @@ jint Java_com_walker_rapidopus_OpusDecoder_decoderGetNbSamples__J_3B(JNIEnv *env
     return ret;
 }
 
-jint Java_com_walker_rapidopus_OpusDecoder_decoderGetNbSamples__JLjava_nio_ByteBuffer_2(JNIEnv *env, jclass jClazz, jlong pDecoder, jobject jInputBuffer) {
+JNIEXPORT jint JNICALL Java_com_walker_rapidopus_OpusDecoder_decoderGetNbSamples__JLjava_nio_ByteBuffer_2(JNIEnv *env, jclass jClazz, jlong pDecoder, jobject jInputBuffer) {
     auto *inData = reinterpret_cast<unsigned char *>(env->GetDirectBufferAddress(jInputBuffer));
     int inSize = env->GetDirectBufferCapacity(jInputBuffer);
 
     return opus_decoder_get_nb_samples(reinterpret_cast<OpusDecoder *>(pDecoder), inData, inSize);
 }
 
-jint Java_com_walker_rapidopus_OpusDecoder_packetGetBandwidth___3B(JNIEnv *env, jclass jClazz, jbyteArray jInputData) {
+JNIEXPORT jint JNICALL Java_com_walker_rapidopus_OpusDecoder_packetGetBandwidth___3B(JNIEnv *env, jclass jClazz, jbyteArray jInputData) {
     jboolean isCopy = JNI_FALSE;
     jbyte *inData = env->GetByteArrayElements(jInputData, &isCopy);
 
@@ -101,13 +101,13 @@ jint Java_com_walker_rapidopus_OpusDecoder_packetGetBandwidth___3B(JNIEnv *env, 
     return ret;
 }
 
-jint Java_com_walker_rapidopus_OpusDecoder_packetGetBandwidth__Ljava_nio_ByteBuffer_2(JNIEnv *env, jclass jClazz, jobject jInputBuffer) {
+JNIEXPORT jint JNICALL Java_com_walker_rapidopus_OpusDecoder_packetGetBandwidth__Ljava_nio_ByteBuffer_2(JNIEnv *env, jclass jClazz, jobject jInputBuffer) {
     auto *inData = reinterpret_cast<unsigned char *>(env->GetDirectBufferAddress(jInputBuffer));
 
     return opus_packet_get_bandwidth(inData);
 }
 
-jint Java_com_walker_rapidopus_OpusDecoder_packetGetNbChannels___3B(JNIEnv *env, jclass jClazz, jbyteArray jInputData) {
+JNIEXPORT jint JNICALL Java_com_walker_rapidopus_OpusDecoder_packetGetNbChannels___3B(JNIEnv *env, jclass jClazz, jbyteArray jInputData) {
     jboolean isCopy = JNI_FALSE;
     jbyte *inData = env->GetByteArrayElements(jInputData, &isCopy);
 
@@ -118,13 +118,13 @@ jint Java_com_walker_rapidopus_OpusDecoder_packetGetNbChannels___3B(JNIEnv *env,
     return ret;
 }
 
-jint Java_com_walker_rapidopus_OpusDecoder_packetGetNbChannels__Ljava_nio_ByteBuffer_2(JNIEnv *env, jclass jClazz, jobject jInputBuffer) {
+JNIEXPORT jint JNICALL Java_com_walker_rapidopus_OpusDecoder_packetGetNbChannels__Ljava_nio_ByteBuffer_2(JNIEnv *env, jclass jClazz, jobject jInputBuffer) {
     auto *inData = reinterpret_cast<unsigned char *>(env->GetDirectBufferAddress(jInputBuffer));
 
     return opus_packet_get_nb_channels(inData);
 }
 
-jint Java_com_walker_rapidopus_OpusDecoder_packetGetNbFrames___3B(JNIEnv *env, jclass jClazz, jbyteArray jInputData) {
+JNIEXPORT jint JNICALL Java_com_walker_rapidopus_OpusDecoder_packetGetNbFrames___3B(JNIEnv *env, jclass jClazz, jbyteArray jInputData) {
     jboolean isCopy = JNI_FALSE;
     int inSize = env->GetArrayLength(jInputData);
     jbyte *inData = env->GetByteArrayElements(jInputData, &isCopy);
@@ -136,14 +136,14 @@ jint Java_com_walker_rapidopus_OpusDecoder_packetGetNbFrames___3B(JNIEnv *env, j
     return ret;
 }
 
-jint Java_com_walker_rapidopus_OpusDecoder_packetGetNbFrames__Ljava_nio_ByteBuffer_2(JNIEnv *env, jclass jClazz, jobject jInputBuffer) {
+JNIEXPORT jint JNICALL Java_com_walker_rapidopus_OpusDecoder_packetGetNbFrames__Ljava_nio_ByteBuffer_2(JNIEnv *env, jclass jClazz, jobject jInputBuffer) {
     auto *inData = reinterpret_cast<unsigned char *>(env->GetDirectBufferAddress(jInputBuffer));
     int inSize = env->GetDirectBufferCapacity(jInputBuffer);
 
     return opus_packet_get_nb_frames(inData, inSize);
 }
 
-jint Java_com_walker_rapidopus_OpusDecoder_packetGetNbSamples___3BI(JNIEnv *env, jclass jClazz, jbyteArray jInputData, jint jSampleRate) {
+JNIEXPORT jint JNICALL Java_com_walker_rapidopus_OpusDecoder_packetGetNbSamples___3BI(JNIEnv *env, jclass jClazz, jbyteArray jInputData, jint jSampleRate) {
     jboolean isCopy = JNI_FALSE;
     int inSize = env->GetArrayLength(jInputData);
     jbyte *inData = env->GetByteArrayElements(jInputData, &isCopy);
@@ -155,14 +155,14 @@ jint Java_com_walker_rapidopus_OpusDecoder_packetGetNbSamples___3BI(JNIEnv *env,
     return ret;
 }
 
-jint Java_com_walker_rapidopus_OpusDecoder_packetGetNbSamples__Ljava_nio_ByteBuffer_2I(JNIEnv *env, jclass jClazz, jobject jInputBuffer, jint jSampleRate) {
+JNIEXPORT jint JNICALL Java_com_walker_rapidopus_OpusDecoder_packetGetNbSamples__Ljava_nio_ByteBuffer_2I(JNIEnv *env, jclass jClazz, jobject jInputBuffer, jint jSampleRate) {
     auto *inData = reinterpret_cast<unsigned char *>(env->GetDirectBufferAddress(jInputBuffer));
     int inSize = env->GetDirectBufferCapacity(jInputBuffer);
 
     return opus_packet_get_nb_samples(inData, inSize, jSampleRate);
 }
 
-jint Java_com_walker_rapidopus_OpusDecoder_packetGetSamplesPerFrame___3BI(JNIEnv *env, jclass jClazz, jbyteArray jInputData, jint jSampleRate) {
+JNIEXPORT jint JNICALL Java_com_walker_rapidopus_OpusDecoder_packetGetSamplesPerFrame___3BI(JNIEnv *env, jclass jClazz, jbyteArray jInputData, jint jSampleRate) {
     jboolean isCopy = JNI_FALSE;
     jbyte *inData = env->GetByteArrayElements(jInputData, &isCopy);
 
@@ -173,14 +173,14 @@ jint Java_com_walker_rapidopus_OpusDecoder_packetGetSamplesPerFrame___3BI(JNIEnv
     return ret;
 }
 
-jint Java_com_walker_rapidopus_OpusDecoder_packetGetSamplesPerFrame__Ljava_nio_ByteBuffer_2I(JNIEnv *env, jclass jClazz, jobject jInputBuffer, jint jSampleRate) {
+JNIEXPORT jint JNICALL Java_com_walker_rapidopus_OpusDecoder_packetGetSamplesPerFrame__Ljava_nio_ByteBuffer_2I(JNIEnv *env, jclass jClazz, jobject jInputBuffer, jint jSampleRate) {
     auto *inData = reinterpret_cast<unsigned char *>(env->GetDirectBufferAddress(jInputBuffer));
     int inSize = env->GetDirectBufferCapacity(jInputBuffer);
 
     return opus_packet_get_samples_per_frame(inData, jSampleRate);
 }
 
-void Java_com_walker_rapidopus_OpusDecoder_nPcmSoftClip___3FII_3F(JNIEnv *env, jclass jClazz, jfloatArray jPcmData, jint jFrameSize, jint jChannels, jfloatArray jMem) {
+JNIEXPORT void JNICALL Java_com_walker_rapidopus_OpusDecoder_nPcmSoftClip___3FII_3F(JNIEnv *env, jclass jClazz, jfloatArray jPcmData, jint jFrameSize, jint jChannels, jfloatArray jMem) {
     jboolean isCopy = JNI_FALSE;
     jfloat *pcmData = env->GetFloatArrayElements(jPcmData, &isCopy);
     jfloat *mem = env->GetFloatArrayElements(jMem, &isCopy);
@@ -191,7 +191,7 @@ void Java_com_walker_rapidopus_OpusDecoder_nPcmSoftClip___3FII_3F(JNIEnv *env, j
     env->ReleaseFloatArrayElements(jPcmData, pcmData, JNI_ABORT);
 }
 
-void Java_com_walker_rapidopus_OpusDecoder_nPcmSoftClip__Ljava_nio_ByteBuffer_2II_3F
+JNIEXPORT void JNICALL Java_com_walker_rapidopus_OpusDecoder_nPcmSoftClip__Ljava_nio_ByteBuffer_2II_3F
     (JNIEnv *env, jclass jClazz, jobject jPcmBuffer, jint jFrameSize, jint jChannels, jfloatArray jMem) {
 
     jboolean isCopy = JNI_FALSE;
@@ -203,7 +203,7 @@ void Java_com_walker_rapidopus_OpusDecoder_nPcmSoftClip__Ljava_nio_ByteBuffer_2I
     env->ReleaseFloatArrayElements(jMem, mem, JNI_ABORT);
 }
 
-void Java_com_walker_rapidopus_OpusDecoder_nPcmSoftClip___3FIILjava_nio_ByteBuffer_2
+JNIEXPORT void JNICALL Java_com_walker_rapidopus_OpusDecoder_nPcmSoftClip___3FIILjava_nio_ByteBuffer_2
     (JNIEnv *env, jclass jClazz, jfloatArray jPcmData, jint jFrameSize, jint jChannels, jobject jMemBuffer) {
 
     jboolean isCopy = JNI_FALSE;
@@ -215,11 +215,49 @@ void Java_com_walker_rapidopus_OpusDecoder_nPcmSoftClip___3FIILjava_nio_ByteBuff
     env->ReleaseFloatArrayElements(jPcmData, pcmData, JNI_ABORT);
 }
 
-void Java_com_walker_rapidopus_OpusDecoder_nPcmSoftClip__Ljava_nio_ByteBuffer_2IILjava_nio_ByteBuffer_2
+JNIEXPORT void JNICALL Java_com_walker_rapidopus_OpusDecoder_nPcmSoftClip__Ljava_nio_ByteBuffer_2IILjava_nio_ByteBuffer_2
     (JNIEnv *env, jclass jClazz, jobject jPcmBuffer, jint jFrameSize, jint jChannels, jobject jMemBuffer) {
 
     auto *pcmData = reinterpret_cast<float *>(env->GetDirectBufferAddress(jPcmBuffer));
     auto *mem = reinterpret_cast<float *>(env->GetDirectBufferAddress(jMemBuffer));
 
     opus_pcm_soft_clip(pcmData, jFrameSize, jChannels, mem);
+}
+
+JNIEXPORT void JNICALL Java_com_walker_rapidopus_OpusDecoder_decoderResetState(JNIEnv *env, jclass jClazz, jlong pDecoder) {
+    opus_decoder_ctl(reinterpret_cast<OpusDecoder *>(pDecoder), OPUS_RESET_STATE);
+}
+
+JNIEXPORT jint JNICALL Java_com_walker_rapidopus_OpusDecoder_decoderGetBandwidth(JNIEnv *env, jclass jClazz, jlong pDecoder) {
+    opus_int32 bw;
+    opus_decoder_ctl(reinterpret_cast<OpusDecoder *>(pDecoder), OPUS_GET_BANDWIDTH(&bw));
+    return bw;
+}
+
+JNIEXPORT jint JNICALL Java_com_walker_rapidopus_OpusDecoder_decoderGetSampleRate(JNIEnv *env, jclass jClazz, jlong pDecoder) {
+    opus_int32 sampleRate;
+    opus_decoder_ctl(reinterpret_cast<OpusDecoder *>(pDecoder), OPUS_GET_SAMPLE_RATE(&sampleRate));
+    return sampleRate;
+}
+
+JNIEXPORT jint JNICALL Java_com_walker_rapidopus_OpusDecoder_decoderGetGain(JNIEnv *env, jclass jClazz, jlong pDecoder) {
+    opus_int32 gain;
+    opus_decoder_ctl(reinterpret_cast<OpusDecoder *>(pDecoder), OPUS_GET_GAIN(&gain));
+    return gain;
+}
+
+JNIEXPORT void JNICALL Java_com_walker_rapidopus_OpusDecoder_decoderSetGain(JNIEnv *env, jclass jClazz, jlong pDecoder, jint jGain) {
+    opus_decoder_ctl(reinterpret_cast<OpusDecoder *>(pDecoder), OPUS_SET_GAIN(jGain));
+}
+
+JNIEXPORT jint JNICALL Java_com_walker_rapidopus_OpusDecoder_decoderGetLastFramePitch(JNIEnv *env, jclass jClazz, jlong pDecoder) {
+    opus_int32 pitch;
+    opus_decoder_ctl(reinterpret_cast<OpusDecoder *>(pDecoder), OPUS_GET_PITCH(&pitch));
+    return pitch;
+}
+
+JNIEXPORT jint JNICALL Java_com_walker_rapidopus_OpusDecoder_decoderGetLastPacketDuration(JNIEnv *env, jclass jClazz, jlong pDecoder) {
+    opus_int32 duration;
+    opus_decoder_ctl(reinterpret_cast<OpusDecoder *>(pDecoder), OPUS_GET_LAST_PACKET_DURATION(&duration));
+    return duration;
 }
--- rapidopus-natives/src/main/cpp/rapidopus.cpp
+++ rapidopus-natives/src/main/cpp/rapidopus.cpp
@@ -1,9 +1,9 @@
 #include <jni.h>
 
 #include "com_walker_rapidopus_RapidOpus.h"
-#include <opus.h>
+#include <opus/opus.h>
 
-jstring Java_com_walker_rapidopus_RapidOpus_getVersion(JNIEnv *env, jclass jClazz) {
+JNIEXPORT jstring JNICALL Java_com_walker_rapidopus_RapidOpus_getVersion(JNIEnv *env, jclass jClazz) {
     const char *versionString = opus_get_version_string();
 
     return env->NewStringUTF(versionString);
